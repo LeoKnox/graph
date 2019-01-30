6166f1a0723d85b541d7abd5d75613e4997003c1
LeoKnox <LeoKnox@users.noreply.github.com>
2019-01-30 14:28:39 -0800

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { checkAndUpdateDirectiveDynamic } from '@angular/core/src/view/provider';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +14,27 @@ export class AppComponent implements OnInit {
   x: number;
   
   ngOnInit(){
-    this.stocks = [{open: "103.1900"}, {open: "101.9000"},{open: "102.5100"}, {open: "105.2600"},{open:"105.0000"}];
+    this.stocks = [{open: "13.1900"}, {open: "101.9000"},{open: "102.5100"}, {open: "105.2600"},{open:"105.0000"}];
     this.points = [50, 150, 100, 23, 150, 98];
-    this.x = 40;
+    //this.x = 40;
     this.newarr = [];
-
-    for (var x = 5; x < 400; x+=10) {
-    //setInterval(function() {
-      this.newarr.push(x);
-      this.newarr.push(Math.floor(Math.random()*70+50));
-    //}, 5000);
+    this.buildData();
+    }
+    
+    buildData() {
+      for (var x = 5; x < 400; x+=10) {
+        this.newarr.push(x);
+        this.newarr.push(Math.floor(Math.random()*70+50));
+      }
+      this.newarr.push(this.newarr[this.newarr.length-2]+10);
+      this.newarr.push(parseInt(this.stocks[0].open));
     }
 
-    
+    addData() {
+      console.log('adding');
+      this.newarr.push(this.newarr[this.newarr.length-2]+10);
+      var up = Math.floor(Math.random()*70+70);
+      this.newarr.push(up);
+      console.log(this.newarr);
   }
-
 }
